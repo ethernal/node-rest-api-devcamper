74966cf6e458d8785e0307e44dfb6d30575e7ac7
Sebastian Pieczyński <testament777@gmail.com>
2019-10-21 12:46:44 +0200

    HTTP Status Codes Example

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const http = require('http');
 
+const todos = [
+  { id: 1, text: `One` },
+  { id: 2, text: `Two` },
+  { id: 3, text: `Three` },
+];
+
 const server = http.createServer((req, res) => {
-  res.end();
+  res.writeHead(404, {
+    'Content-Type': 'application/json',
+    'X-Powered-By': 'Node.js',
+  });
+
+  res.end(
+    JSON.stringify({
+      success: false,
+      error: 'Please fill out email..',
+      data: null,
+    })
+  );
 });
 
 const PORT = 5000;
