601cf363e3f787bf6ad9220d46e991184add60fc
Sebastian Pieczyński <testament777@gmail.com>
2019-10-23 10:53:39 +0200

    feat: Use morgan instead of custom logger

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const dotenv = require(`dotenv`);
-const logger = require(`./middleware/logger`);
+const morgan = require(`morgan`);
 
 // Route Files
 
@@ -11,7 +11,9 @@ const bootcamps = require(`./routes/bootcamps`);
 dotenv.config({ path: `./config/config.env` });
 
 const app = express();
-app.use(logger);
+
+// Dev Logging middleware
+if (process.env.NODE_ENV === `development`) app.use(morgan(`dev`));
 
 // Mount routes
 app.use(`/api/v1/bootcamps`, bootcamps);
