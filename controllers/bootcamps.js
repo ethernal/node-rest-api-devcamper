148566d5aa622f14c3de532a480f806c6e26f432
Sebastian Pieczyński <testament777@gmail.com>
2019-10-25 12:12:51 +0200

    Convert errors to use custom Error Response

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require(`../models/Bootcamp`);
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
@@ -24,14 +25,21 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
-      // MUST return as we will get an error saying that Headers are alreay sent:
+      // MUST return as we will get an error saying that Headers are already sent:
       // Error: Cannot set headers after they are sent to the client
-      return res.status(404).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (error) {
-    next(error);
+    next(
+      new ErrorResponse(
+        `Error while fetching Bootcamp with id of ${req.params.id}`,
+        400
+      )
+    );
   }
 };
 
