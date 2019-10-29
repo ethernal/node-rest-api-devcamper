c2ad29cf1ad4c58b551f9533e448565913115c65
Sebastian Pieczyński <testament777@gmail.com>
2019-10-29 15:55:32 +0100

    feat: Average Course cost calculation for Bootcamp

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,35 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+// Define a static function in a schema for Average Cost
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  const aggregateObject = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(aggregateObject[0].averageCost / 10) * 10,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Call getAverageCost after save
+
+CourseSchema.post(`save`, function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+CourseSchema.pre(`remove`, function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model(`Course`, CourseSchema);
