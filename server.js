689804db29d2c86fbf14d2e8b6e44ca21d9cdfa2
Sebastian Pieczyński <testament777@gmail.com>
2019-10-30 10:17:12 +0100

    fix: fileupload middleware blocking requests

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === `development`) {
   app.use(morgan(`dev`));
 }
 // File uploading middleware
-app.use(fileupload);
+app.use(fileupload());
 
 //Set static folder
 app.use(express.static(path.join(__dirname, `public`)));
