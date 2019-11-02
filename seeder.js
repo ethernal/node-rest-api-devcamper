91853976670d940d676558106add36bb1362c39e
Sebastian Pieczyński <testament777@gmail.com>
2019-11-02 19:14:32 +0100

    feat: Seeder can import and delete Users

--- seeder.js
+++ seeder.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: `./config/config.env` });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 const dbConnectionOptions = {
   connectTimeoutMS: 1000,
@@ -31,6 +32,9 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, `utf-8`)
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, `utf-8`)
+);
 
 // Import into DB
 
@@ -40,8 +44,13 @@ const importData = async () => {
 
     await Bootcamp.create(bootcamps);
     console.log(`Bootcamps imported`.green.inverse);
+
     await Course.create(courses);
     console.log(`Courses imported`.green.inverse);
+
+    await User.create(users);
+    console.log(`Bootcamps imported`.green.inverse);
+
     await mongoose.disconnect();
     console.log(`MongoDB disconnected.`.green.inverse);
     process.exit();
@@ -58,6 +67,8 @@ const deleteData = async () => {
     console.log("Bootcaps deleted".red.inverse);
     await Course.deleteMany();
     console.log("Courses deleted".red.inverse);
+    await User.deleteMany();
+    console.log("Users deleted".red.inverse);
     await mongoose.disconnect();
     console.log(`MongoDB disconnected.`.green.inverse);
     process.exit();
