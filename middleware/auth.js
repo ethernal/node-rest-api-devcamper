4e1e0682ea4bd7c81debc041c4632d8116fd8e67
Sebastian Pieczyński <testament777@gmail.com>
2019-11-03 18:34:53 +0100

    feat: Autologin / Remember me with cookie & logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,11 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer ")
   ) {
+    // Set token from Headers
     token = req.headers.authorization.replace("Bearer ", "");
   }
-  //   else if (req.cookies.token) {
-  //       token = req.cookies.token;
-  //     }
+  // Set token from Cookie
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
 
   // Make sure token exists
   if (!token) {
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require(`express`);
 const {
   register,
   login,
+  logout,
   getLoggedInUser,
   resetPasswordRequest,
   resetPassword,
@@ -17,6 +18,7 @@ const { protect } = require(`../middleware/auth`);
 router.get(`/me`, protect, getLoggedInUser);
 router.post(`/register`, register);
 router.post(`/login`, login);
+router.post(`/logout`, protect, logout);
 router.post(`/update-user-data`, protect, updateUserData);
 router.post(`/reset-password`, resetPasswordRequest);
 router.put(`/reset-password/:resetToken`, resetPassword);
