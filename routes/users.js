bb54af35a3b853123656722be7d859666a148c41
Sebastian Pieczyński <testament777@gmail.com>
2019-11-03 15:54:17 +0100

    feat: Users CRUD functionality for admins

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require(`express`);
+const User = require(`../models/User`);
+
+const {
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require(`../controllers/users`);
+
+const router = express.Router();
+
+const advancedResults = require(`../middleware/advancedResults`);
+const { protect, authorize } = require(`../middleware/auth`);
+
+// this will make router use protect and admin authorization for all routes below
+router.use(protect);
+router.use(authorize(`admin`));
+
+router
+  .route(`/`)
+  .get(advancedResults(User), getUsers)
+  .post(createUser);
+
+router
+  .route(`/:id`)
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
