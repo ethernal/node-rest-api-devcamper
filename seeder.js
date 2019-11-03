72421aedd5c3cd5da170391e192997d4f56d8b3d
Sebastian Pieczyński <testament777@gmail.com>
2019-11-03 16:40:06 +0100

    feat: Import and Destroy Review data

--- seeder.js
+++ seeder.js
@@ -6,6 +6,7 @@ dotenv.config({ path: `./config/config.env` });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
 const dbConnectionOptions = {
   connectTimeoutMS: 1000,
@@ -35,6 +36,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, `utf-8`)
 );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, `utf-8`)
+);
 
 // Import into DB
 
@@ -49,7 +53,10 @@ const importData = async () => {
     console.log(`Courses imported`.green.inverse);
 
     await User.create(users);
-    console.log(`Bootcamps imported`.green.inverse);
+    console.log(`Users imported`.green.inverse);
+
+    await Review.create(reviews);
+    console.log(`Reviews imported`.green.inverse);
 
     await mongoose.disconnect();
     console.log(`MongoDB disconnected.`.green.inverse);
@@ -65,12 +72,19 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     console.log("Bootcaps deleted".red.inverse);
+
     await Course.deleteMany();
     console.log("Courses deleted".red.inverse);
+
     await User.deleteMany();
     console.log("Users deleted".red.inverse);
+
+    await Review.deleteMany();
+    console.log("Users deleted".red.inverse);
+
     await mongoose.disconnect();
     console.log(`MongoDB disconnected.`.green.inverse);
+
     process.exit();
   } catch (error) {
     console.error(error);
